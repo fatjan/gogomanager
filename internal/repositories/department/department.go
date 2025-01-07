4f8a5450aeccc6229de286c35cd497ca6bfe6b77
Fatma <fatma.janna.utp@gmail.com>
2025-01-07 11:48:59 +0700

    add department repo

---
+++ internal/repositories/department/department.go
@@ -0,0 +1,19 @@
+package department
+
+import (
+	"github.com/fatjan/gogomanager/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type repository struct {
+	db *sqlx.DB
+}
+
+func NewDepartmentRepository(db *sqlx.DB) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) Post(department *models.Department) error {
+	_, err := r.db.Exec("INSERT INTO public.department (id, name) VALUES ($1, $2)", department.ID, department.Name)
+	return err
+}
