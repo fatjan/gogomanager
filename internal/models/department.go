0ff717d8de72efc4bb7411da5003294ca6e30ede
afnan <afnan@fastratabuana.co.id>
2025-01-08 10:25:24 +0700

    hotfix : add CreatedAt and UpdatedAt in model department

--- internal/models/department.go
+++ internal/models/department.go
@@ -5,7 +5,7 @@ import "time"
 type Department struct {
 	ID        int       `db:"id"`
 	Name      string    `db:"name"`
-	ManagerID int       `db:"Manager_id"`
+	ManagerID int       `db:"manager_id"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
