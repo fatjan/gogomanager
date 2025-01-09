aced31543dd4b891c229872ee0a1c9a4fc88f389
Ainur Rofiq <89074435+rizqrofiq@users.noreply.github.com>
2025-01-09 13:39:53 +0700

    feat post/employee (#8)
    * employee get endpoint
    
    * keep changes
    
    * fix models and dto
    
    * add check for limit and offset
    
    * update query to use wildcard
    
    * invalid limit and offset will use the default value
    
    * case insensitive
    
    * remove employeeImageURI from employeeRequest fields
    
    * feat: post employee
    
    * fix: update manager_id table references
    
    * fix: identityNumber per departmentId constraint
    
    * feat: use manager_id from context in post employee
    
    ---------
    
    Co-authored-by: Fatma <fatma.janna.utp@gmail.com>

--- internal/repositories/employee/intf.go
+++ internal/repositories/employee/intf.go
@@ -1,8 +1,11 @@
 package employee
 
-import "github.com/fatjan/gogomanager/internal/dto"
-import "github.com/fatjan/gogomanager/internal/models"
+import (
+	"github.com/fatjan/gogomanager/internal/dto"
+	"github.com/fatjan/gogomanager/internal/models"
+)
 
 type Repository interface {
 	GetAll(*dto.EmployeeRequest) ([]*models.Employee, error)
+	Post(*models.Employee) (*models.Employee, error)
 }
