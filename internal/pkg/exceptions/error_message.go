9aa41ac8c5ac1cccb7a0171885810d508c103924
Fatma Janna <46950501+fatjan@users.noreply.github.com>
2025-01-11 21:59:22 +0700

    make invalid input as bad request status code 400 (#19)

--- internal/pkg/exceptions/error_message.go
+++ internal/pkg/exceptions/error_message.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrNotFound = errors.New("Your requested Item is not found")
 	ErrConflict = errors.New("Your Item already exist")
+	ErrorBadRequest = errors.New("Invalid field(s) on request")
 )
